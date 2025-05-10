ccefa462f79e064bbb553a4428800835f12b00eb
Melanya25 <o.melanya@mail.ru>
2025-05-10 17:28:47 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,73 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "mongoose/mongoose.h"
-#include "constants.h"
+#include "mongoose.h"
 
 static const char *s_http_port = "8080";
 
-// Новая сигнатура функции обратного вызова
-static void handle_request(struct mg_connection *c, int ev, void *ev_data) {
+// Новая сигнатура обработчика
+static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
     if (ev == MG_EV_HTTP_MSG) {
         struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+        
         if (mg_http_match_uri(hm, "/")) {
-            // Отправляем страницу с формой
-            mg_http_reply(c, 200, "Content-Type: text/html\r\n", "%s", 
-                "<!DOCTYPE html><html lang=\"ru\"><head><meta charset=\"UTF-8\" /><title>Создать поздравление</title></head><body>"
-                "<h1>Создать поздравление</h1>"
-                "<form method=\"POST\" action=\"/submit\">"
-                "<label for=\"name\">Имя:</label>"
-                "<input type=\"text\" id=\"name\" name=\"name\" required><br><br>"
-                "<label for=\"occasion\">Повод:</label>"
-                "<select id=\"occasion\" name=\"occasion\">"
-                "<option value=\"День рождения\">День рождения</option>"
-                "<option value=\"Новый год\">Новый год</option>"
-                "<option value=\"Выпускной\">Выпускной</option>"
-                "</select><br><br>"
-                "<input type=\"submit\" value=\"Поздравить\" />"
-                "</form>"
-                "</body></html>");
-        } else if (mg_http_match_uri(hm, "/submit")) {
-            // Получаем параметры формы
-            char name[50] = {0};
-            char occasion[50] = {0};
-
-            // Парсим параметры из тела запроса
-            struct mg_str *body = &hm->body;
-            char *body_str = malloc(body->len + 1);
-            if (body_str) {
-                snprintf(body_str, body->len + 1, "%.*s", (int)body->len, body->buf);
-                
-                // Парсим name
-                char *p = strstr(body_str, "name=");
-                if (p) {
-                    p += 5;
-                    char *amp = strchr(p, '&');
-                    if (amp) *amp = '\0';
-                    mg_url_decode(p, strlen(p), name, sizeof(name), 0);
-                }
-                
-                // Парсим occasion
-                p = strstr(body_str, "occasion=");
-                if (p) {
-                    p += 9;
-                    char *amp = strchr(p, '&');
-                    if (amp) *amp = '\0';
-                    mg_url_decode(p, strlen(p), occasion, sizeof(occasion), 0);
-                }
-                free(body_str);
-            }
-
-            // Формируем ответ
+            // Главная страница с формой
             mg_http_reply(c, 200, "Content-Type: text/html\r\n",
-                "<!DOCTYPE html><html lang=\"ru\"><head><meta charset=\"UTF-8\" /><title>Поздравление</title></head><body>"
+                "<!DOCTYPE html><html><head><title>Конструктор</title></head><body>"
+                "<h1>Создать поздравление</h1>"
+                "<form method='POST' action='/submit'>"
+                "<label>Имя: <input name='name' required></label><br>"
+                "<label>Повод: <select name='occasion'>"
+                "<option value='День рождения'>День рождения</option>"
+                "<option value='Новый год'>Новый год</option>"
+                "<option value='Выпускной'>Выпускной</option>"
+                "</select></label><br>"
+                "<button>Поздравить</button></form></body></html>");
+        }
+        else if (mg_http_match_uri(hm, "/submit")) {
+            // Обработка формы
+            char name[50] = "", occasion[50] = "";
+            
+            // Парсинг параметров
+            mg_http_get_var(&hm->body, "name", name, sizeof(name));
+            mg_http_get_var(&hm->body, "occasion", occasion, sizeof(occasion));
+            
+            // Ответ с поздравлением
+            mg_http_reply(c, 200, "Content-Type: text/html\r\n",
+                "<!DOCTYPE html><html><head><title>Поздравление</title></head><body>"
                 "<h1>Поздравление для %s</h1>"
-                "<p>Поздравляю тебя с %s!</p>"
-                "<p>Желаю тебе всего самого лучшего!</p>"
-                "<a href=\"/\">Создать новое поздравление</a>"
-                "</body></html>", name, occasion);
-        } else {
-            mg_http_reply(c, 404, "", "Страница не найдена");
+                "<p>С %s!</p><p>Всего наилучшего!</p>"
+                "<a href='/'>Назад</a></body></html>",
+                name, occasion);
+        }
+        else {
+            mg_http_reply(c, 404, NULL, "Not found");
         }
     }
 }
@@ -76,19 +50,11 @@ int main(void) {
     struct mg_mgr mgr;
     mg_mgr_init(&mgr);
     
-    // Убрали последний параметр NULL, так как сигнатура изменилась
-    struct mg_connection *c = mg_http_listen(&mgr, s_http_port, handle_request);
+    // Создаем HTTP-сервер
+    mg_http_listen(&mgr, s_http_port, fn, NULL);
     
-    if (c == NULL) {
-        printf("Ошибка запуска сервера\n");
-        return 1;
-    }
-    
-    printf("Сервер запущен на порту %s\n", s_http_port);
-    
-    while (true) {
-        mg_mgr_poll(&mgr, 1000);
-    }
+    printf("Server running on http://localhost:%s\n", s_http_port);
+    for (;;) mg_mgr_poll(&mgr, 1000);
     
     mg_mgr_free(&mgr);
     return 0;

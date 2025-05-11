2e6c3bd73342c0510da6c7544e061f28737e6421
Melanya25 <o.melanya@mail.ru>
2025-05-11 12:10:21 +0300

    Update input.h

--- input/input.h
+++ input/input.h
@@ -1,8 +1,12 @@
 #ifndef INPUT_H
 #define INPUT_H
+
 #include <stddef.h>
 
+// Function to parse form parameters
 int parse_form_param(const char *body, const char *param_name, char *output, size_t maxlen);
 
+// URL decoding function
 int url_decode(const char *src, char *dest, size_t maxlen);
-#endif // INPUT_H
+
+#endif

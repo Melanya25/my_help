8c95f41871ed07119f61f43f5dae25eca83eb21a
Melanya25 <o.melanya@mail.ru>
2025-05-11 12:09:56 +0300

    Update constants.h

--- constants/constants.h
+++ constants/constants.h
@@ -1,15 +1,18 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
-#define TITLE "Конструктор поздравлений"
+
+#define TITLE "Greeting Card Generator"
+
 #define FORM_HTML "<form method=\"POST\" action=\"/submit\">" \
-                  "<label for=\"name\">Имя:</label>" \
-                  "<input type=\"text\" id=\"name\" name=\"name\" required><br><br>" \
-                  "<label for=\"occasion\">Повод:</label>" \
-                  "<select id=\"occasion\" name=\"occasion\">" \
-                  "<option value=\"День рождения\">День рождения</option>" \
-                  "<option value=\"Новый год\">Новый год</option>" \
-                  "<option value=\"Выпускной\">Выпускной</option>" \
-                  "</select><br><br>" \
-                  "<input type=\"submit\" value=\"Поздравить\">" \
-                  "</form>"
-#endif // CONSTANTS_H
+"<label for=\"name\">Recipient Name:</label>" \
+"<input type=\"text\" id=\"name\" name=\"name\" required><br><br>" \
+"<label for=\"occasion\">Occasion:</label>" \
+"<select id=\"occasion\" name=\"occasion\">" \
+"<option value=\"Birthday\">Birthday</option>" \
+"<option value=\"New Year\">New Year</option>" \
+"<option value=\"Graduation\">Graduation</option>" \
+"</select><br><br>" \
+"<input type=\"submit\" value=\"Generate Greeting\">" \
+"</form>"
+
+#endif
